8f3c0b23788b84856a9f01a85ba844cd9ab43c19
fchntst <91413895+fchntst@users.noreply.github.com>
2023-05-14 15:08:50 +0200

    Add files via upload

---
+++ First Programs/maximum_again.py
@@ -0,0 +1,8 @@
+list_numbers = [1, 6, 3]
+
+maximum = list_numbers[0]
+if list_numbers[1] > maximum:
+    maximum = list_numbers[1]
+if list_numbers[2] > maximum:
+    maximum = list_numbers[2]
+print(maximum)
