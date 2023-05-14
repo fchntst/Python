5ee0eabf6e3fb67b8b3905905fa124663db11078
fchntst <91413895+fchntst@users.noreply.github.com>
2023-05-14 15:26:41 +0200

    Add files via upload

---
+++ Lists Tuples/sequence.py
@@ -0,0 +1,8 @@
+numbers = (1, 2, 3, 6, 3, 2, 3, 3)
+print("Lenth: " + str(len(numbers)))
+print("Minimum: " + str(min(numbers)))
+print("Maximum: " + str(max(numbers)))
+print('3s: ' + str(numbers.count(3)))
+print("Position of 3: " + str(numbers.index(3,5)))
+# to the index method: (3: find the number 3, 5: from the fiveth position)
+
