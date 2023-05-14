ed7a4c9e130455deea35dbe49ea361c404a06df0
fchntst <91413895+fchntst@users.noreply.github.com>
2023-05-14 14:34:28 +0200

    Create conversions.py

---
+++ Lists Tuples/conversions.py
@@ -0,0 +1,15 @@
+my_list = [1, 2, 3]
+my_tuple = tuple(my_list)
+print(my_tuple)
+
+
+my_tuple = (1, 2, 3)
+my_list = list(my_tuple)
+print(my_list)
+
+my_list = list(range(5, 26, 5))
+print(my_list)
+
+msg = "hello world"
+my_list = list(msg)
+print(my_list)
