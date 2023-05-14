03f9db3843fefe6db0c6044a2128ae014c67536b
fchntst <91413895+fchntst@users.noreply.github.com>
2023-05-14 15:38:41 +0200

    Add files via upload

---
+++ Cool programs/type_data.py
@@ -0,0 +1,10 @@
+print(type(1))
+print(type(1.2))
+print(type(True))
+print(type('hello'))
+my_list = [1, 2]
+print(type(my_list))
+my_tuple = (1, 3, 5)
+print(type(my_tuple))
+my_set = {1, 3, 6}
+print(type(my_set))
