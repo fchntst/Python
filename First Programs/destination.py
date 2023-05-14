9f2b065b49548c4467623c118905f595c48c01de
fchntst <91413895+fchntst@users.noreply.github.com>
2023-05-14 14:35:33 +0200

    Add files via upload

---
+++ First Programs/destination.py
@@ -0,0 +1,10 @@
+destination = "somewhere"
+print('i want to go outside today')
+
+weather = 'shiny'
+
+if weather == "rainy":
+    print("i won't go outside cuse it's raining")
+
+else:
+    print('i go outside')
